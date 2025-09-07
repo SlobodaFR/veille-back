a74cfdc035787369590637692b1b71ac24a4d5fe
Thomas SLOBODA <thomas@sloboda.fr>
2025-09-07 18:52:01 +0200

    chore: initialize project configuration files and linting setup

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+export default {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-max-length': [0, 'always', 1000],
+        'body-max-line-length': [0, 'always', Infinity],
+        'footer-max-line-length': [0, 'always', Infinity],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'chore',
+                'style',
+                'refactor',
+                'ci',
+                'test',
+                'revert',
+                'perf',
+                'vercel',
+            ],
+        ],
+    },
+};
