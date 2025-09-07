4af401a09507e1707e658367316c22a7a4cf6583
Thomas SLOBODA <thomas@sloboda.fr>
2025-09-07 23:24:24 +0200

    chore: update Vitest configuration to include source files for coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
         coverage: {
             provider: 'v8',
             reporter: ['text', 'json', 'html'],
-            exclude: ['node_modules/', 'dist/', 'src/**/*.spec.ts'],
+            include: ['src/**/*.ts'],
+            exclude: ['src/**/*.spec.ts', 'src/**/index.ts'],
         },
     },
     resolve: {
